9792712d6b013f0bbe9478b4e8974b03119157c7
teddy8587 <teddy.barieraud@gmail.com>
2026-06-04 22:37:27 +0200

    Fix mobile layout issues in forms, filters and navigation

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -47,12 +47,13 @@ function SearchBar({
 
   return (
     <div hidden={!isResearchOpen} className="header__research">
-        <legend className="sr-only">Tri des résultats</legend>
-        <div
-          className={`header__input--research ${
-            isResearchOpen ? "header__checkboxAnnonce" : "header__link--hidden"
-          }`}
-        >
+      <legend className="sr-only">Tri des résultats</legend>
+      <div
+        className={`header__input--research ${
+          isResearchOpen ? "header__checkboxAnnonce" : "header__link--hidden"
+        }`}
+      >
+        <div className="header__filters">
           <label htmlFor="sort-date">
             <input
               id="sort-date"
@@ -98,27 +99,11 @@ function SearchBar({
             ordre alphabétique (Z → A)
           </label>
         </div>
-        <div>
-          {token && role === "admin" && (
-            <label
-              htmlFor="only-reported"
-              className={`header__input--research  ${
-                isResearchOpen
-                  ? "header__checkboxAnnonce"
-                  : "header__link--hidden"
-              }`}
-            >
-              <input
-                id="only-reported"
-                type="checkbox"
-                checked={onlyReported}
-                onChange={(e) => setOnlyReported(e.target.checked)}
-              />
-              Afficher uniquement les profils signalés
-            </label>
-          )}
+      </div>
+      <div className="header__filters">
+        {token && role === "admin" && (
           <label
-            htmlFor="only-photo"
+            htmlFor="only-reported"
             className={`header__input--research  ${
               isResearchOpen
                 ? "header__checkboxAnnonce"
@@ -126,142 +111,153 @@ function SearchBar({
             }`}
           >
             <input
-              id="only-photo"
+              id="only-reported"
               type="checkbox"
-              checked={onlyWithPhoto}
-              onChange={(e) => setOnlyWithPhoto(e.target.checked)}
+              checked={onlyReported}
+              onChange={(e) => setOnlyReported(e.target.checked)}
             />
-            Afficher les profils avec photo
+            Afficher uniquement les profils signalés
           </label>
-          <label
-            htmlFor="only-audio"
-            className={`header__input--research  ${
-              isResearchOpen
-                ? "header__checkboxAnnonce"
-                : "header__link--hidden"
+        )}
+        <label
+          htmlFor="only-photo"
+          className={`header__input--research  ${
+            isResearchOpen ? "header__checkboxAnnonce" : "header__link--hidden"
+          }`}
+        >
+          <input
+            id="only-photo"
+            type="checkbox"
+            checked={onlyWithPhoto}
+            onChange={(e) => setOnlyWithPhoto(e.target.checked)}
+          />
+          Afficher les profils avec photo
+        </label>
+        <label
+          htmlFor="only-audio"
+          className={`header__input--research  ${
+            isResearchOpen ? "header__checkboxAnnonce" : "header__link--hidden"
+          }`}
+        >
+          <input
+            id="only-audio"
+            type="checkbox"
+            checked={onlyWithAudio}
+            onChange={(e) => setOnlyWithAudio(e.target.checked)}
+          />
+          Afficher les profils avec audio
+        </label>
+        <label
+          htmlFor="only-annonce"
+          className={`header__input--research  ${
+            isResearchOpen ? "header__checkboxAnnonce" : "header__link--hidden"
+          }`}
+        >
+          <input
+            id="only-annonce"
+            type="checkbox"
+            checked={onlyWithAnnonce}
+            onChange={(e) => setOnlyWithAnnonce(e.target.checked)}
+          />
+          Afficher les profils avec une annonce
+        </label>
+      </div>
+      <div className="header__research--flex">
+        <button
+          className="header__button"
+          onClick={toggleResearch}
+          aria-label="Ouvrir la recherche"
+        >
+          <i
+            className="fa-solid fa-magnifying-glass"
+            style={{ color: "white" }}
+          ></i>
+        </button>
+        <div className="header__nav--research">
+          <label htmlFor="search-name" className="sr-only">
+            Recherche par nom
+          </label>
+          <input
+            id="search-name"
+            type="text"
+            placeholder="par Nom"
+            className={`header__input--research ${
+              isResearchOpen ? "" : "header__link--hidden"
             }`}
-          >
-            <input
-              id="only-audio"
-              type="checkbox"
-              checked={onlyWithAudio}
-              onChange={(e) => setOnlyWithAudio(e.target.checked)}
-            />
-            Afficher les profils avec audio
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <label htmlFor="style-select" className="sr-only">
+            Filtrer par style
           </label>
-          <label
-            htmlFor="only-annonce"
-            className={`header__input--research  ${
-              isResearchOpen
-                ? "header__checkboxAnnonce"
-                : "header__link--hidden"
+          <select
+            id="style-select"
+            className={`header__input--research ${
+              isResearchOpen ? "" : "header__link--hidden"
             }`}
+            value={selectedStyle}
+            onChange={(e) => setSelectedStyle(e.target.value)}
           >
-            <input
-              id="only-annonce"
-              type="checkbox"
-              checked={onlyWithAnnonce}
-              onChange={(e) => setOnlyWithAnnonce(e.target.checked)}
-            />
-            Afficher les profils avec une annonce
+            {selectedStyle === "" && (
+              <option value="" disabled hidden>
+                Par style
+              </option>
+            )}
+            <option value="">Tous les styles</option>
+            {allStyles.map((style) => (
+              <option key={style} value={style}>
+                {style}
+              </option>
+            ))}
+          </select>
+          <label htmlFor="instrument-select" className="sr-only">
+            Filtrer par instrument
           </label>
+          <select
+            className={`header__input--research ${
+              isResearchOpen ? "" : "header__link--hidden"
+            }`}
+            id="instrument-select"
+            value={selectedInstrument}
+            onChange={(e) => setSelectedInstrument(e.target.value)}
+          >
+            {selectedInstrument === "" && (
+              <option value="" disabled hidden>
+                Par instruments
+              </option>
+            )}
+            <option value="">Tous les instruments</option>
+            {allInstruments.map((instrument) => (
+              <option key={instrument} value={instrument}>
+                {instrument}
+              </option>
+            ))}
+          </select>
+          <label htmlFor="ville-select" className="sr-only">
+            Filtrer par ville
+          </label>
+          <select
+            id="ville-select"
+            className={`header__input--research ${
+              isResearchOpen ? "" : "header__link--hidden"
+            }`}
+            value={selectedVille}
+            onChange={(e) => setSelectedVille(e.target.value)}
+          >
+            {selectedVille === "" && (
+              <option value="" disabled hidden>
+                Par ville
+              </option>
+            )}
+            <option value="">Toutes les villes</option>
+            {allVilles.map((ville) => (
+              <option key={ville} value={ville}>
+                {ville}
+              </option>
+            ))}
+          </select>
         </div>
-        <div className="header__research--flex">
-          <button
-            className="header__button"
-            onClick={toggleResearch}
-            aria-label="Ouvrir la recherche"
-          >
-            <i
-              className="fa-solid fa-magnifying-glass"
-              style={{ color: "white" }}
-            ></i>
-          </button>
-          <div className="header__nav--research">
-            <label htmlFor="search-name" className="sr-only">
-              Recherche par nom
-            </label>
-            <input
-              id="search-name"
-              type="text"
-              placeholder="par Nom"
-              className={`header__input--research ${
-                isResearchOpen ? "" : "header__link--hidden"
-              }`}
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            <label htmlFor="style-select" className="sr-only">
-              Filtrer par style
-            </label>
-            <select
-              id="style-select"
-              className={`header__input--research ${
-                isResearchOpen ? "" : "header__link--hidden"
-              }`}
-              value={selectedStyle}
-              onChange={(e) => setSelectedStyle(e.target.value)}
-            >
-              {selectedStyle === "" && (
-                <option value="" disabled hidden>
-                  Par style
-                </option>
-              )}
-              <option value="">Tous les styles</option>
-              {allStyles.map((style) => (
-                <option key={style} value={style}>
-                  {style}
-                </option>
-              ))}
-            </select>
-            <label htmlFor="instrument-select" className="sr-only">
-              Filtrer par instrument
-            </label>
-            <select
-              className={`header__input--research ${
-                isResearchOpen ? "" : "header__link--hidden"
-              }`}
-              id="instrument-select"
-              value={selectedInstrument}
-              onChange={(e) => setSelectedInstrument(e.target.value)}
-            >
-              {selectedInstrument === "" && (
-                <option value="" disabled hidden>
-                  Par instruments
-                </option>
-              )}
-              <option value="">Tous les instruments</option>
-              {allInstruments.map((instrument) => (
-                <option key={instrument} value={instrument}>
-                  {instrument}
-                </option>
-              ))}
-            </select>
-            <label htmlFor="ville-select" className="sr-only">
-              Filtrer par ville
-            </label>
-            <select
-              id="ville-select"
-              className={`header__input--research ${
-                isResearchOpen ? "" : "header__link--hidden"
-              }`}
-              value={selectedVille}
-              onChange={(e) => setSelectedVille(e.target.value)}
-            >
-              {selectedVille === "" && (
-                <option value="" disabled hidden>
-                  Par ville
-                </option>
-              )}
-              <option value="">Toutes les villes</option>
-              {allVilles.map((ville) => (
-                <option key={ville} value={ville}>
-                  {ville}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
+      </div>
     </div>
   );
 }
